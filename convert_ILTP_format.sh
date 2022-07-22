d35eca9e2b464d94562ecc7647de227ef9364e08
0314R <0314yio@gmail.com>
2022-07-22 17:02:59 +0200

    updated converter, outputs to separate folder; added script to run ATP

--- convert_ILTP_format.sh
+++ convert_ILTP_format.sh
@@ -1,9 +1,17 @@
 #! /bin/sh
 make -C ILTP_format_converter;
+
+mkdir -p ILTP_CONVERTED/LCL;
+mkdir -p ILTP_CONVERTED/SYJ;
+mkdir -p ILTP_CONVERTED/SYN;
+
 for f in ILTP/*/*.p; do
-	basename=${f%".p"};
-	ILTP_format_converter/converter< "${basename}.p" > "${basename}.pl";
+	extensionless=${f%".p"}; 					#remove extension from filename
+	sub_and_file=${extensionless#"ILTP/"}; 		#remove directory ILTP/ from filename (but not subdirectory)
+	ILTP_format_converter/converter< "ILTP/${sub_and_file}.p" > "ILTP_CONVERTED/${sub_and_file}.pl";
  	done
+
+# cleaning up
 cd ILTP_format_converter;
 rm converter converter.tab.c converter.tab.h lex.yy.c;
 cd ..;
